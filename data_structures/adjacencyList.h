39adf4bd51168969cd4f4dec2a8c7a9046460e83
AlessandroPegoraro <spampego@gmail.com>
2020-04-05 21:29:55 +0200

    Implementato Union(edge) e aggiunta funzione di stampa e calcolo dei pesi totali

--- data_structures/adjacencyList.h
+++ data_structures/adjacencyList.h
@@ -7,27 +7,50 @@
 
 #include <vector>
 #include <utility>
-//#include "data_structures/Edge.h"
+#include <iostream>
 
 template <typename T,typename W>
 class AdjacencyList {
 private:
     std::vector< std::vector< std::pair<T,W> > > array;
+    bool DFS_inside(T v,T w,bool L[])const;
 public:
     explicit AdjacencyList(int n);
     explicit AdjacencyList(int n, const std::vector<Edge<T,W>>&);
     explicit AdjacencyList(int n,std::vector<Edge<T,W>>&&);
     [[nodiscard]] unsigned int size() const;
+    [[nodiscard]] int totalCost() const;
     void unite(Edge<T,W> ed);
-    bool DFS(T v,T w,bool L[])const;
+    bool DFS(T v,T w)const;
+    template <typename U>
+    friend std::ostream& operator<<(std::ostream &os, const AdjacencyList<U,U>& ad);
 };
+
+/*private methods*/
+
+/*
+ * Same as DSF, we just do not create the list of visited node L[n], but receive it from DFS()
+ */
+template <typename T,typename W>
+bool AdjacencyList<T, W>::DFS_inside(T v, T w, bool L[]) const {
+    L[v] = true; //preemptively set that the node v has been visited, to skip next call of DFS_inside(v,...)
+    for(int i=0; i < array[v].size(); ++i){ //cycle all adjacent node of v
+        if(L[array[v][i].first] == 0){ //if we have not check before this particular adjacent node of v then
+            if(array[v][i].first == w){ // if the adjacent node is the searched node w then v and w are connected
+                return true;
+            }else{
+                if(DFS_inside(array[v][i].first, w, L) != 0){ // search all adjacent node of this adjacent node
+                    return true; // it exist at least one path from this adjacent node of v to the searched node w
+                }
+            }
+        }
+    }
+    return false; // we have searched all nodes, and a path between v and w does not exist
+}
 
 /*public methods*/
 template <typename T,typename W>
-AdjacencyList<T,W>::AdjacencyList(int n): array(n, std::vector<std::pair<T,W>>()){//{std::make_pair(NULL,NULL)}
-    /*for(int i= 0; i<array.size(); ++i){
-        array[i].pop_back();
-    }*/
+AdjacencyList<T,W>::AdjacencyList(int n): array(n, std::vector<std::pair<T,W>>()){
 }
 
 template<typename T, typename W>
@@ -60,9 +83,22 @@ unsigned int AdjacencyList<T,W>::size() const {
     return array.size();
 }
 
+
+template<typename T, typename W>
+int AdjacencyList<T, W>::totalCost() const {
+    int cost = 0;
+    for(const auto & i : array){
+        for(const auto & j : i){
+            cost += j.second;
+        }
+    }
+    return cost / 2;
+}
+
 template<typename T, typename W>
 void AdjacencyList<T, W>::unite(Edge<T,W> ed){
-    array[0].push_back(std::make_pair(ed.get_node_2(), ed.get_weight()));
+    array[ed.get_node_1()].push_back(std::make_pair(ed.get_node_2(), ed.get_weight()));
+    array[ed.get_node_2()].push_back(std::make_pair(ed.get_node_1(), ed.get_weight()));
 }
 
 /* DFS fast implementation:
@@ -71,14 +107,15 @@ void AdjacencyList<T, W>::unite(Edge<T,W> ed){
  * Return True if it exists a path between v and w, False otherwise.
  */
 template<typename T, typename W>
-bool AdjacencyList<T, W>::DFS(T v, T w, bool L[]) const {
-    L[v] = true; //preemptively set that the node v has been visited, to skip next call of DFS(v,...)
+bool AdjacencyList<T, W>::DFS(T v, T w) const {
+    bool L[array.size()] = {0};
+    L[v] = true; //preemptively set that the node v has been visited, to skip next call of DFS_inside(v,...)
     for(int i=0; i < array[v].size(); ++i){ //cycle all adjacent node of v
         if(L[array[v][i].first] == 0){ //if we have not check before this particular adjacent node of v then
             if(array[v][i].first == w){ // if the adjacent node is the searched node w then v and w are connected
                 return true;
             }else{
-                if(DFS(array[v][i].first, w, L) != 0){ // search all adjacent node of this adjacent node
+                if(DFS_inside(array[v][i].first, w, L) != 0){ // search all adjacent node of this adjacent node
                     return true; // it exist at least one path from this adjacent node of v to the searched node w
                 }
             }
@@ -87,5 +124,18 @@ bool AdjacencyList<T, W>::DFS(T v, T w, bool L[]) const {
     return false; // we have searched all nodes, and a path between v and w does not exist
 }
 
+template<typename U>
+std::ostream& operator<<(std::ostream& os, const AdjacencyList<U,U>& ad){
+    os << "Number of nodes   : " << ad.array.size() << std::endl;
+    os << "Cost of all edges : " << ad.totalCost() << std::endl;
+    for(int i = 0; i < ad.array.size(); ++i){
+        os << i+1 << " :";
+        for(int j = 0; j < ad.array[i].size(); ++j){
+            os << " ( " << ad.array[i][j].first+1 << " , " << ad.array[i][j].second << " )";
+        }
+        os << std::endl;
+    }
+    return os;
+}
 
 #endif //ALGADVGRAPHS_ADJACENCYLIST_H
